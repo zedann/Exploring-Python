cd96acd52fae58447531bae1d43e299f4734f88a
Zedan <zedamohamed144@gmail.com>
2023-10-29 19:28:19 +0200

    learn about errors , files and modules

---
+++ errors_files.py
@@ -0,0 +1,27 @@
+# try:
+#     val = int(input('enter a number:'))
+#     print(val)
+# except ZeroDivisionError:
+#     print('can not divid by zero')
+# except:
+#     print('please enter valid input')
+
+'''
++ : read and write
+w : write
+r : read
+a : append
+
+*w truncate the file then write
+'''
+# file = open('file.txt','a+')
+# # print(type(file.read()))
+# # print(file.readline())
+# # print(file.readlines())
+# # for line in file.readlines():
+# #     print(line)
+# file.write('\nand i love hunter x hunter anime')
+# file.close()
+
+import docx
+print(docx)
